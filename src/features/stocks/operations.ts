6cad92ba6030369789059d51ae48c8ee2c46ce07
軽部勝仁 <karube5217@gmail.com>
2020-02-11 23:49:21 +0900

    Refactor Thunk types

--- src/features/stocks/operations.ts
+++ src/features/stocks/operations.ts
@@ -1,5 +1,5 @@
 import { AnyAction } from "redux";
-import { ThunkAction, ThunkDispatch } from "redux-thunk";
+import { ThunkAction } from "redux-thunk";
 import { createInstance } from "@src/utils/request";
 import {
   getStocksRequest,
@@ -28,139 +28,127 @@ export const getStocksAsync = (): ThunkAction<
   {},
   undefined,
   AnyAction
-> => {
-  return async (dispatch: ThunkDispatch<{}, {}, AnyAction>): Promise<void> => {
-    dispatch(getStocksRequest());
-    const request = createInstance();
-    request({
-      method: "get",
-      url: "/api/stocks/"
+> => dispatch => {
+  dispatch(getStocksRequest());
+  const request = createInstance();
+  request({
+    method: "get",
+    url: "/api/stocks/"
+  })
+    .then(({ data }) => {
+      const reversed = data.stocks.reverse();
+      dispatch(getStocksSuccess(reversed));
     })
-      .then(({ data }) => {
-        const reversed = data.stocks.reverse();
-        dispatch(getStocksSuccess(reversed));
-      })
-      .catch(err => {
-        console.log(err.message);
-        dispatch(getStocksFail());
-      });
-  };
+    .catch(err => {
+      console.log(err.message);
+      dispatch(getStocksFail());
+    });
 };
 
 export const createStockAsync = (
   data: FormData
-): ThunkAction<void, {}, undefined, AnyAction> => {
-  return async (dispatch: ThunkDispatch<{}, {}, AnyAction>): Promise<void> => {
-    dispatch(createStockRequest());
-    const request = createInstance();
-    request({
-      method: "post",
-      url: "/api/stocks/",
-      data: {
-        ...data
-      }
+): ThunkAction<void, {}, undefined, AnyAction> => dispatch => {
+  dispatch(createStockRequest());
+  const request = createInstance();
+  request({
+    method: "post",
+    url: "/api/stocks/",
+    data: {
+      ...data
+    }
+  })
+    .then(({ data }) => {
+      dispatch(createStockSuccess(data.stock));
     })
-      .then(({ data }) => {
-        dispatch(createStockSuccess(data.stock));
-      })
-      .catch(err => {
-        console.log(err.message);
-        dispatch(createStockFail());
-      });
-  };
+    .catch(err => {
+      console.log(err.message);
+      dispatch(createStockFail());
+    });
 };
 
 export const addStockAsync = (
   data: FormData
-): ThunkAction<void, {}, undefined, AnyAction> => {
-  return async (dispatch: ThunkDispatch<{}, {}, AnyAction>): Promise<void> => {
-    dispatch(addStockRequest());
-    const request = createInstance();
-    request({
-      method: "patch",
-      url: "/api/stocks/",
-      data: {
-        ...data
-      }
+): ThunkAction<void, {}, undefined, AnyAction> => dispatch => {
+  dispatch(addStockRequest());
+  const request = createInstance();
+  request({
+    method: "patch",
+    url: "/api/stocks/",
+    data: {
+      ...data
+    }
+  })
+    .then(({ data }) => {
+      dispatch(addStockSuccess(data.stock));
     })
-      .then(({ data }) => {
-        dispatch(addStockSuccess(data.stock));
-      })
-      .catch(err => {
-        console.log(err.message);
-        dispatch(addStockFail());
-      });
-  };
+    .catch(err => {
+      console.log(err.message);
+      dispatch(addStockFail());
+    });
 };
 
-export const updateStockAsync = (
+export const updateStockAsync: (
   data: FormData
-): ThunkAction<void, {}, undefined, AnyAction> => {
-  return async (dispatch: ThunkDispatch<{}, {}, AnyAction>): Promise<void> => {
-    dispatch(updateStockRequest());
-    const request = createInstance();
-    request({
-      method: "patch",
-      url: "/api/stocks/",
-      data: {
-        ...data
-      }
+) => ThunkAction<void, {}, undefined, AnyAction> = data => dispatch => {
+  dispatch(updateStockRequest());
+  const request = createInstance();
+  request({
+    method: "patch",
+    url: "/api/stocks/",
+    data: {
+      ...data
+    }
+  })
+    .then(({ data }) => {
+      dispatch(updateStockSuccess(data.stock));
     })
-      .then(({ data }) => {
-        dispatch(updateStockSuccess(data.stock));
-      })
-      .catch(err => {
-        console.log(err.message);
-        dispatch(updateStockFail());
-      });
-  };
+    .catch(err => {
+      console.log(err.message);
+      dispatch(updateStockFail());
+    });
 };
 
 export const deleteStockAsync = (
   stockId: any
-): ThunkAction<void, {}, undefined, AnyAction> => {
-  return async (dispatch: ThunkDispatch<{}, {}, AnyAction>): Promise<void> => {
-    dispatch(deleteStockRequest());
-    const request = createInstance();
-    request({
-      method: "delete",
-      url: "/api/stocks/",
-      data: {
-        stockId
-      }
+): ThunkAction<void, {}, undefined, AnyAction> => dispatch => {
+  dispatch(deleteStockRequest());
+  const request = createInstance();
+  request({
+    method: "delete",
+    url: "/api/stocks/",
+    data: {
+      stockId
+    }
+  })
+    .then(({ data }) => {
+      dispatch(deleteStockSuccess(data.stockId));
     })
-      .then(({ data }) => {
-        dispatch(deleteStockSuccess(data.stockId));
-      })
-      .catch(err => {
-        console.log(err.message);
-        dispatch(deleteStockFail());
-      });
-  };
+    .catch(err => {
+      console.log(err.message);
+      dispatch(deleteStockFail());
+    });
 };
 
 export const reorderStocksAsync = (
   data: any
-): ThunkAction<void, {}, undefined, AnyAction> => {
-  return async (dispatch: ThunkDispatch<{}, {}, AnyAction>): Promise<void> => {
-    const stocks = data.map((item: any) => {
-      return item.id;
-    });
-    dispatch(reorderStocksRequest());
-    const request = createInstance();
-    request({
-      method: "patch",
-      url: "/api/stocks/reorder",
-      data: {
-        stocks
-      }
+): ThunkAction<void, {}, undefined, AnyAction> => dispatch => {
+  const stocks = data.map((item: any) => {
+    return item.id;
+  });
+  dispatch(reorderStocksRequest());
+  const request = createInstance();
+  request({
+    method: "patch",
+    url: "/api/stocks/reorder",
+    data: {
+      stocks
+    }
+  })
+    .then(_res => {
+      dispatch(reorderStocksSuccess());
     })
-      .then(_res => {
-        dispatch(reorderStocksSuccess());
-      })
-      .catch(err => {
-        console.log(err.message);
-        dispatch(reorderStocksFail());
-      });
-  };
+    .catch(err => {
+      console.log(err.message);
+      dispatch(reorderStocksFail());
+    });
 };
