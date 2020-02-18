07ad156aa8854a0981218e9c91a79a50da67815f
finalize <shgysd@gmail.com>
2020-02-19 03:37:43 +0900

    Reset store

--- src/app/rootReducer.ts
+++ src/app/rootReducer.ts
@@ -5,9 +5,18 @@ import stocks from "@src/features/stocks";
 import notes from "@src/features/notes";
 import profile from "@src/features/profile";
 
-export default combineReducers({
+import { States } from "./types";
+
+const appReducer = combineReducers({
   auth,
   stocks,
   notes,
   profile
 });
+
+export default (state: States | undefined, action: any) => {
+  if (action.type === "RESET_STORE") {
+    state = undefined;
+  }
+  return appReducer(state, action);
+};
